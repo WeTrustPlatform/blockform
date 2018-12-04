8490f1f7b4ff8cf8609ac2c7a171a059488f35ca
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-12-04 13:31:28 +0700

    Fix ssh login and user creation

--- cloudinit/main.go
+++ cloudinit/main.go
@@ -4,11 +4,13 @@ import (
 	"encoding/base64"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 
 	"github.com/WeTrustPlatform/blockform/model"
 )
 
+// CustomData returns the base64 encoded cloud-init script to initialize a VM
 func CustomData(node model.Node) string {
 	var data []byte
 	switch node.NetworkID {
@@ -26,7 +28,10 @@ func CustomData(node model.Node) string {
 
 	str := string(data)
 	str = strings.Replace(str, "@@API_KEY@@", node.APIKey, -1)
+	str = strings.Replace(str, "@@PUB_KEY@@", os.Getenv("PUB_KEY"), -1)
 	str = strings.Replace(str, "@@NET_ID@@", fmt.Sprintf("%d", node.NetworkID), -1)
+
+	fmt.Println(str)
 
 	return base64.StdEncoding.EncodeToString([]byte(str))
 }
