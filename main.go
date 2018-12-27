d4bd3a028233aeed8df65af4f11e3e552d4e83e1
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-12-27 15:04:53 +0700

    Adapt database model and templates

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
@@ -81,15 +80,15 @@ func serviceLogs(ctx context.Context, node model.Node, unit string, callback fun
 
 func setupHTTPS(ctx context.Context, node model.Node, domain string, email string, onSuccess func(), onError func(error)) {
 	cmd := `sudo certbot certonly --nginx -d ` + domain + ` --agree-tos --non-interactive --email ` + email + ` && sudo sed -i -e "s|listen 8545;|server_name ` + domain + `;listen [::]:8545 ssl ipv6only=on;listen 8545 ssl;ssl_certificate /etc/letsencrypt/live/` + domain + `/fullchain.pem;ssl_certificate_key /etc/letsencrypt/live/` + domain + `/privkey.pem;include /etc/letsencrypt/options-ssl-nginx.conf;ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;|g" /etc/nginx/sites-available/default && sudo sed -i -e "s|listen 8546;|server_name ` + domain + `;listen [::]:8546 ssl ipv6only=on;listen 8546 ssl;ssl_certificate /etc/letsencrypt/live/` + domain + `/fullchain.pem;ssl_certificate_key /etc/letsencrypt/live/` + domain + `/privkey.pem;include /etc/letsencrypt/options-ssl-nginx.conf;ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;|g" /etc/nginx/sites-available/default && sudo systemctl restart nginx`
-	fmt.Println(cmd)
-	stdin, stderr, err := sshcmd.Exec(
+
+	_, _, err := sshcmd.Exec(
 		os.Getenv("PRIV_KEY"),
 		os.Getenv("PASSPHRASE"),
 		"blockform",
 		node.DomainName,
 		cmd,
 	)
-	fmt.Println(stdin, stderr)
+
 	if err != nil {
 		log.Println(err)
 		onError(err)
@@ -274,8 +273,6 @@ func main() {
 		domain := r.FormValue("domain")
 		email := r.FormValue("email")
 
-		fmt.Println(ID, domain, email)
-
 		// TODO validate form input
 
 		node := model.Node{}
@@ -283,9 +280,10 @@ func main() {
 
 		setupHTTPS(context.Background(), node, domain, email,
 			func() {
-				w.Write([]byte("success"))
 				node.DomainName = domain
+				node.HasSSL = true
 				db.Save(&node)
+				http.Redirect(w, r, "/node/"+ID+"/endpoints", http.StatusSeeOther)
 			},
 			func(err error) {
 				w.Write([]byte(err.Error()))
